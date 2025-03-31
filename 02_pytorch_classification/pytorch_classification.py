049399b98191a0b5280a4d130b52631d4bc01733
Flint92 <1992flint@gmail.com>
2025-03-31 15:02:40 +0800

    pytorch classification

--- 02_pytorch_classification/pytorch_classification.py
+++ 02_pytorch_classification/pytorch_classification.py
@@ -17,6 +17,17 @@ class CircleModelV0(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.layer_2(self.layer_1(x))
 
+class CircleModelV1(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features=2, out_features=10)
+        self.layer_2 = nn.Linear(in_features=10, out_features=10)
+        self.layer_3 = nn.Linear(in_features=10, out_features=1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
+
 
 def accuracy_fn(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
@@ -59,23 +70,31 @@ if __name__ == '__main__':
 
 
     device = get_device()
-    # model_0 = CircleModelV0().to(device)
-    # print(model_0.state_dict())
+    # model = CircleModelV0().to(device)
+    # model = CircleModelV1().to(device)
+    # print(model.state_dict())
 
 
-    model_0 = nn.Sequential(
-        nn.Linear(in_features=2, out_features=5),
-        nn.Linear(in_features=5, out_features=1),
+    # model = nn.Sequential(
+    #     nn.Linear(in_features=2, out_features=5),
+    #     nn.Linear(in_features=5, out_features=1),
+    # ).to(device)
+    # print(model.state_dict())
+    model = nn.Sequential(
+        nn.Linear(in_features=2, out_features=10),
+        nn.ReLU(),
+        nn.Linear(in_features=10, out_features=10),
+        nn.ReLU(),
+        nn.Linear(in_features=10, out_features=1),
     ).to(device)
-    # print(model_0.state_dict())
 
     loss_fn = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.SGD(params=model_0.parameters(),
+    optimizer = torch.optim.SGD(params=model.parameters(),
                                 lr=0.1)
 
     # with torch.inference_mode():
-    #     model_0.eval()
-    #     y_logits = model_0(X_test.to(device))[:5]
+    #     model.eval()
+    #     y_logits = model(X_test.to(device))[:5]
     #     y_pred_probs = torch.sigmoid(y_logits)
     #     y_preds = torch.round(y_pred_probs)
     #     y_pred_labels = torch.round(torch.sigmoid(model_0(X_test.to(device))[:5]))
@@ -85,11 +104,11 @@ if __name__ == '__main__':
     torch.mps.manual_seed(42)  # set the seed for MPS
     torch.cuda.manual_seed(42)  # set the seed for CUDA
 
-    epoches = 180
+    epoches = 1000
     for epoch in range(epoches):
-        model_0.train()
+        model.train()
 
-        y_logits = model_0(X_train.to(device)).squeeze()
+        y_logits = model(X_train.to(device)).squeeze()
         y_preds = torch.round(torch.sigmoid(y_logits))
 
         loss = loss_fn(y_logits, y_train.to(device))
@@ -101,9 +120,9 @@ if __name__ == '__main__':
 
         optimizer.step()
 
-        model_0.eval()
+        model.eval()
         with torch.inference_mode():
-            test_logits = model_0(X_test.to(device)).squeeze()
+            test_logits = model(X_test.to(device)).squeeze()
             test_preds = torch.round(torch.sigmoid(test_logits))
             test_loss = loss_fn(test_logits, y_test.to(device))
             test_acc = accuracy_fn(y_test.to(device), test_preds)
@@ -115,9 +134,9 @@ if __name__ == '__main__':
     plt.figure(figsize=(12, 6))
     plt.subplot(1, 2, 1)
     plt.title('Train')
-    plot_decision_boundary(model_0, X_train, y_train)
+    plot_decision_boundary(model, X_train, y_train)
     plt.subplot(1, 2, 2)
     plt.title('Test')
-    plot_decision_boundary(model_0, X_test, y_test)
+    plot_decision_boundary(model, X_test, y_test)
     plt.show()
 
